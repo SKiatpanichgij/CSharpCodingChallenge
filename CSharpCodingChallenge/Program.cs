e5fccb264d77839ae05316ab4462123d4e09a4b6
DESKTOP-62KTP00\ADMIN <sutipong_k@hotmail.com>
2025-10-03 10:16:33 +0700

    add OldPhonePad and Class Program

--- CSharpCodingChallenge/Program.cs
+++ CSharpCodingChallenge/Program.cs
@@ -1,4 +1,78 @@
 ﻿using System;
 
-// See https://aka.ms/new-console-template for more information 
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main(string[] args)
+    {
+        /// Test cases for oldPhonePad 
+        Console.WriteLine("Testing OldPhonePad conversion:");
+
+        // Example test cases
+        Console.WriteLine(OldPhonePad("33#")); // Output: E
+        Console.WriteLine(OldPhonePad("227*#")); // Output: B
+        Console.WriteLine(OldPhonePad("4433555 555666#")); // Output: HELLO
+        Console.WriteLine(OldPhonePad("8 88777444666*664#")); // Output: ????
+    }
+
+    public static string OldPhonePad(string input)
+    {
+        string[] keypad = { " ", "", "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ" };
+        string result = "";
+        int count = 0;
+        char lastChar = '\0';
+
+        foreach (char c in input)
+        {
+            if (c == '#') break;
+
+            if (c == '*')
+            {
+                count = 0; // Reset count when deleting
+                lastChar = '\0'; // Reset lastChar when deleting
+                continue;
+            }
+
+            if (c == ' ')
+            {
+                if (lastChar != '\0' && count > 0)
+                {
+                    int index = lastChar - '0';
+                    if (index >= 0 && index < keypad.Length)
+                    {
+                        result += keypad[index][(count - 1) % keypad[index].Length];
+                    }
+                }
+                lastChar = c;
+                count = 0; 
+            }
+            else if (c == lastChar)
+            {
+                count++;
+            }
+            else
+            {
+                if (lastChar != '\0' && count > 0)
+                {
+                    int index = lastChar - '0';
+                    if (index >= 0 && index < keypad.Length)
+                    {
+                        result += keypad[index][(count - 1) % keypad[index].Length];
+                    }
+                }
+                lastChar = c;
+                count = 1;
+            }
+        }
+
+        if (lastChar != '\0' && count > 0)
+        {
+            int index = lastChar - '0';
+            if (index >= 0 && index < keypad.Length)
+            {
+                result += keypad[index][(count - 1) % keypad[index].Length];
+            }
+        }
+
+        return result;
+    }
+}
--- CSharpCodingChallenge/obj/Debug/net9.0/CSharpCodingChallenge.AssemblyInfo.cs
+++ CSharpCodingChallenge/obj/Debug/net9.0/CSharpCodingChallenge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharpCodingChallenge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dc8b44064758ab4972d421271118df80374fb98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b69f94eaefbd835db6762ea48ec01b6774c6e656")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharpCodingChallenge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharpCodingChallenge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
