14347b503be3e76cb1521f88542858693318125c
DESKTOP-62KTP00\ADMIN <sutipong_k@hotmail.com>
2025-10-05 03:43:19 +0700

    add the initial test project - dotnet test at termminal

--- CSharpCodingChallenge/obj/Debug/net9.0/CSharpCodingChallenge.AssemblyInfo.cs
+++ CSharpCodingChallenge/obj/Debug/net9.0/CSharpCodingChallenge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharpCodingChallenge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7da43c929f113405ad84a989f1dafb3ba4101de0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b860cbcaaf9ec91911de0bb3cffc65ea129bcb9b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharpCodingChallenge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharpCodingChallenge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿namespace TestProject;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test()
+    {
+        /// Test cases for oldPhonePad 
+        Console.WriteLine("Testing OldPhonePad conversion:");
+
+        // Example test case
+        Assert.Equal("E", MyNameSpace.MyMethodClass.OldPhonePad("33#")); // Output: E
+        Assert.Equal("B", MyNameSpace.MyMethodClass.OldPhonePad("227*#")); // Output: B
+        Assert.Equal("HELLO", MyNameSpace.MyMethodClass.OldPhonePad("4433555 555666#")); // Output: HELLO
+        Assert.Equal("TURING", MyNameSpace.MyMethodClass.OldPhonePad("8 88777444666*664#")); // Output: ????
+
+    }
+}
